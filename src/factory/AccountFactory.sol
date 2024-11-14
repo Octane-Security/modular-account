521e9786159b7d3893fbcf99c4ed0f175565d8ba
howy <132113803+howydev@users.noreply.github.com>
2024-11-15 06:20:24 +0700

    feat: add function to deploy webauthn accounts from factory (#275)
    Co-authored-by: Jay Paik <jay@alchemy.com>
    Co-authored-by: zer0dot <zer0dot.dev@gmail.com>

--- src/factory/AccountFactory.sol
+++ src/factory/AccountFactory.sol
@@ -20,9 +20,13 @@ contract AccountFactory is Ownable {
     SemiModularAccountBytecode public immutable SEMI_MODULAR_ACCOUNT_IMPL;
     IEntryPoint public immutable ENTRY_POINT;
     address public immutable SINGLE_SIGNER_VALIDATION_MODULE;
+    address public immutable WEBAUTHN_VALIDATION_MODULE;
 
     event ModularAccountDeployed(address indexed account, address indexed owner, uint256 salt);
     event SemiModularAccountDeployed(address indexed account, address indexed owner, uint256 salt);
+    event WebAuthnModularAccountDeployed(
+        address indexed account, uint256 indexed ownerX, uint256 indexed ownerY, uint256 salt
+    );
 
     error TransferFailed();
 
@@ -31,21 +35,25 @@ contract AccountFactory is Ownable {
         ModularAccount _accountImpl,
         SemiModularAccountBytecode _semiModularImpl,
         address _singleSignerValidationModule,
+        address _webAuthnValidationModule,
         address owner
     ) Ownable(owner) {
         ENTRY_POINT = _entryPoint;
         ACCOUNT_IMPL = _accountImpl;
         SEMI_MODULAR_ACCOUNT_IMPL = _semiModularImpl;
         SINGLE_SIGNER_VALIDATION_MODULE = _singleSignerValidationModule;
+        WEBAUTHN_VALIDATION_MODULE = _webAuthnValidationModule;
     }
 
-    /**
-     * Create an account, and return its address.
-     * Returns the address even if the account is already deployed.
-     * Note that during user operation execution, this method is called only if the account is not deployed.
-     * This method returns an existing account address so that entryPoint.getSenderAddress() would work even after
-     * account creation
-     */
+    /// @notice Create an account with the single singer validation module installed, and return its address.
+    /// @dev Returns the address even if the account is already deployed.
+    /// Note that during user operation execution, this method is called only if the account is not deployed.
+    /// This method returns an existing account address so that entryPoint.getSenderAddress() would work even after
+    /// account creation.
+    /// @param owner The owner of the account.
+    /// @param salt The salt to use for the account creation.
+    /// @param entityId The entity ID to use for the account creation.
+    /// @return The address of the created account.
     function createAccount(address owner, uint256 salt, uint32 entityId) external returns (ModularAccount) {
         bytes32 combinedSalt = getSalt(owner, salt, entityId);
 
@@ -69,8 +77,12 @@ contract AccountFactory is Ownable {
         return ModularAccount(payable(instance));
     }
 
-    /// @dev This only ever deploys semi-modular accounts with added bytecode, since this is much less
+    /// @notice Create a semi-modular account and return its address.
+    /// @dev This only ever deploys semi-modular accounts with added bytecode since this is much less
     /// expensive than the storage-only variant, which should only be used for upgrades.
+    /// @param owner The owner of the account.
+    /// @param salt The salt to use for the account creation.
+    /// @return The address of the created account.
     function createSemiModularAccount(address owner, uint256 salt) external returns (SemiModularAccountBytecode) {
         // both module address and entityId for fallback validations are hardcoded at the maximum value.
         bytes32 fullSalt = getSalt(owner, salt, type(uint32).max);
@@ -88,23 +100,64 @@ contract AccountFactory is Ownable {
         return SemiModularAccountBytecode(payable(instance));
     }
 
+    /// @notice Create an account with the WebAuthn module installed, and return its address.
+    /// @dev Returns the address even if the account is already deployed.
+    /// Note that during user operation execution, this method is called only if the account is not deployed.
+    /// This method returns an existing account address so that entryPoint.getSenderAddress() would work even after
+    /// account creation.
+    /// @param ownerX The x coordinate of the owner's public key.
+    /// @param ownerY The y coordinate of the owner's public key.
+    /// @param salt The salt to use for the account creation.
+    /// @param entityId The entity ID to use for the account creation.
+    /// @return The address of the created account.
+    function createWebAuthnAccount(uint256 ownerX, uint256 ownerY, uint256 salt, uint32 entityId)
+        external
+        returns (ModularAccount)
+    {
+        bytes32 combinedSalt = getSaltWebAuthn(ownerX, ownerY, salt, entityId);
+
+        // LibClone short-circuits if it's already deployed.
+        (bool alreadyDeployed, address instance) =
+            LibClone.createDeterministicERC1967(address(ACCOUNT_IMPL), combinedSalt);
+
+        // short circuit if exists
+        if (!alreadyDeployed) {
+            bytes memory moduleInstallData = abi.encode(entityId, ownerX, ownerY);
+            // point proxy to actual implementation and init plugins
+            ModularAccount(payable(instance)).initializeWithValidation(
+                ValidationConfigLib.pack(WEBAUTHN_VALIDATION_MODULE, entityId, true, true, true),
+                new bytes4[](0),
+                moduleInstallData,
+                new bytes[](0)
+            );
+            emit WebAuthnModularAccountDeployed(instance, ownerX, ownerY, salt);
+        }
+
+        return ModularAccount(payable(instance));
+    }
+
+    /// @notice Add stake to the entry point contract.
+    /// @param unstakeDelay The delay in seconds before the stake can be withdrawn.
     function addStake(uint32 unstakeDelay) external payable onlyOwner {
         ENTRY_POINT.addStake{value: msg.value}(unstakeDelay);
     }
 
+    /// @notice Unlock the stake in the entry point contract.
     function unlockStake() external onlyOwner {
         ENTRY_POINT.unlockStake();
     }
 
+    /// @notice Withdraw the stake from the entry point contract.
+    /// @param withdrawAddress The address to withdraw the stake to.
     function withdrawStake(address payable withdrawAddress) external onlyOwner {
         ENTRY_POINT.withdrawStake(withdrawAddress);
     }
 
-    /// @notice Withdraw funds from this contract
-    /// @dev can withdraw stuck erc20s or native currency
-    /// @param to address to send erc20s or native currency to
-    /// @param token address of the token to withdraw, 0 address for native currency
-    /// @param amount amount of the token to withdraw in case of rebasing tokens
+    /// @notice Withdraw funds from this contract.
+    /// @dev Can be used to withdraw native currency or ERC-20 tokens.
+    /// @param to The address to withdraw the funds to.
+    /// @param token The address of the token to withdraw, or the zero address for native currency.
+    /// @param amount The amount to withdraw.
     function withdraw(address payable to, address token, uint256 amount) external onlyOwner {
         if (token == address(0)) {
             (bool success,) = to.call{value: address(this).balance}("");
@@ -116,23 +169,67 @@ contract AccountFactory is Ownable {
         }
     }
 
-    /**
-     * Calculate the counterfactual address of this account as it would be returned by createAccount()
-     */
+    /// @notice Calculate the counterfactual address of this account as it would be returned by createAccount.
+    /// @param owner The owner of the account.
+    /// @param salt The salt to use for the account creation.
+    /// @param entityId The entity ID to use for the account creation.
+    /// @return The address of the account.
     function getAddress(address owner, uint256 salt, uint32 entityId) external view returns (address) {
         return LibClone.predictDeterministicAddressERC1967(
             address(ACCOUNT_IMPL), getSalt(owner, salt, entityId), address(this)
         );
     }
 
+    /// @notice Calculate the counterfactual address of a semi-modular account as it would be returned by
+    /// createSemiModularAccount.
+    /// @param owner The owner of the account.
+    /// @param salt The salt to use for the account creation.
+    /// @return The address of the account.
     function getAddressSemiModular(address owner, uint256 salt) public view returns (address) {
         bytes32 fullSalt = getSalt(owner, salt, type(uint32).max);
         bytes memory immutables = _getImmutableArgs(owner);
         return _getAddressSemiModular(immutables, fullSalt);
     }
 
+    /// @notice Calculate the counterfactual address of a webauthn account as it would be returned by
+    /// createWebAuthnAccount.
+    /// @param ownerX The x coordinate of the owner's public key.
+    /// @param ownerY The y coordinate of the owner's public key.
+    /// @param salt The salt to use for the account creation.
+    /// @param entityId The entity ID to use for the account creation.
+    /// @return The address of the account.
+    function getAddressWebAuthn(uint256 ownerX, uint256 ownerY, uint256 salt, uint32 entityId)
+        public
+        view
+        returns (address)
+    {
+        return LibClone.predictDeterministicAddressERC1967(
+            address(ACCOUNT_IMPL), getSaltWebAuthn(ownerX, ownerY, salt, entityId), address(this)
+        );
+    }
+
+    /// @notice Get the full salt used for account creation.
+    /// @dev To get the full salt used in createSemiModularAccount, use type(uint32).max for entityId.
+    /// @param owner The owner of the account.
+    /// @param salt The salt to use for the account creation.
+    /// @param entityId The entity ID to use for the account creation.
+    /// @return The full salt.
     function getSalt(address owner, uint256 salt, uint32 entityId) public pure returns (bytes32) {
         return keccak256(abi.encodePacked(owner, salt, entityId));
+    }
+
+    /// @notice Get the full salt used for account creation using WebAuthn.
+    /// @param ownerX The x coordinate of the owner's public key.
+    /// @param ownerY The y coordinate of the owner's public key.
+    /// @param salt The salt to use for the account creation.
+    /// @param entityId The entity ID to use for the account creation.
+    /// @return The full salt.
+    function getSaltWebAuthn(uint256 ownerX, uint256 ownerY, uint256 salt, uint32 entityId)
+        public
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encodePacked(ownerX, ownerY, salt, entityId));
     }
 
     function _getAddressSemiModular(bytes memory immutables, bytes32 salt) internal view returns (address) {
--- test/factory/AccountFactory.t.sol
+++ test/factory/AccountFactory.t.sol
@@ -26,9 +26,18 @@ import {MockERC20} from "../mocks/MockERC20.sol";
 
 contract AccountFactoryTest is AccountTestBase {
     MockERC20 public erc20;
+    uint256 internal _ownerX = 1;
+    uint256 internal _ownerY = 2;
 
     function test_createAccount() public withSMATest {
         ModularAccount account = factory.createAccount(address(this), 100, TEST_DEFAULT_VALIDATION_ENTITY_ID);
+
+        assertEq(address(account.entryPoint()), address(entryPoint));
+    }
+
+    function test_createWebAuthnAccount() public {
+        ModularAccount account =
+            factory.createWebAuthnAccount(_ownerX, _ownerY, 100, TEST_DEFAULT_VALIDATION_ENTITY_ID);
 
         assertEq(address(account.entryPoint()), address(entryPoint));
     }
@@ -39,6 +48,25 @@ contract AccountFactoryTest is AccountTestBase {
         assertEq(
             address(account), address(factory.createAccount(address(this), 100, TEST_DEFAULT_VALIDATION_ENTITY_ID))
         );
+
+        assertEq(
+            address(account), address(factory.getAddress(address(this), 100, TEST_DEFAULT_VALIDATION_ENTITY_ID))
+        );
+    }
+
+    function test_createWebAuthnAccountAndGetAddress() public {
+        ModularAccount account =
+            factory.createWebAuthnAccount(_ownerX, _ownerY, 100, TEST_DEFAULT_VALIDATION_ENTITY_ID);
+
+        assertEq(
+            address(account),
+            address(factory.createWebAuthnAccount(_ownerX, _ownerY, 100, TEST_DEFAULT_VALIDATION_ENTITY_ID))
+        );
+
+        assertEq(
+            address(account),
+            address(factory.getAddressWebAuthn(_ownerX, _ownerY, 100, TEST_DEFAULT_VALIDATION_ENTITY_ID))
+        );
     }
 
     function test_multipleDeploy() public withSMATest {
@@ -47,6 +75,23 @@ contract AccountFactoryTest is AccountTestBase {
         uint256 startGas = gasleft();
 
         ModularAccount account2 = factory.createAccount(address(this), 100, TEST_DEFAULT_VALIDATION_ENTITY_ID);
+
+        // Assert that the 2nd deployment call cost less than 1 sstore
+        // Implies that no deployment was done on the second calls
+        assertLe(startGas - 22_000, gasleft());
+
+        // Assert the return addresses are the same
+        assertEq(address(account), address(account2));
+    }
+
+    function test_multipleDeployWebAuthn() public {
+        ModularAccount account =
+            factory.createWebAuthnAccount(_ownerX, _ownerY, 100, TEST_DEFAULT_VALIDATION_ENTITY_ID);
+
+        uint256 startGas = gasleft();
+
+        ModularAccount account2 =
+            factory.createWebAuthnAccount(_ownerX, _ownerY, 100, TEST_DEFAULT_VALIDATION_ENTITY_ID);
 
         // Assert that the 2nd deployment call cost less than 1 sstore
         // Implies that no deployment was done on the second calls
